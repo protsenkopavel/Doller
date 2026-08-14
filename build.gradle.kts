7e2055ef0b155da5538a623123f97378919c0f9c
Pavel Protsenko <pavelprotsenko@outlook.com>
2026-08-15 02:54:06 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.1.0"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "doller"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+}
